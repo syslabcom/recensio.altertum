4603985d42e912d6f04ca970e47453a23b2e87da
Manuel Reinhardt <reinhardt@syslab.com>
2016-05-13 09:49:16 +0000

    Custom generate-pdf-recension view, refs #13564

---
+++ recensio/altertum/browser/views.py
@@ -0,0 +1,7 @@
+from recensio.theme.browser.pdfgen import GeneratePdfRecension
+
+
+class GeneratePdfRecensionAltertum(GeneratePdfRecension):
+    """Customized cover page
+    """
+    logo_main = '++resource++recensio.altertum.images/mainimage_de.png'
