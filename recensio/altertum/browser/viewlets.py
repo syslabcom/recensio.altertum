86d8d97040b3dafb320ba5746c84bcc68c1d876b
Manuel Reinhardt <reinhardt@syslab.com>
2018-02-23 08:21:54 +0000

    Removed obsolete commenting viewlet. Refs #16221

--- recensio/altertum/browser/viewlets.py
+++
@@ -1,6 +0,0 @@
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from recensio.policy.browser.comments import CommentsViewlet as BaseCommentsViewlet
-
-
-class CommentsViewlet(BaseCommentsViewlet):
-    index = ViewPageTemplateFile('templates/comments.pt')
