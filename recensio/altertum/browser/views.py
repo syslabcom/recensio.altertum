69fd5638cbbe62040481403147a4bf3f4ea9ddef
Johannes Raggam <thetetet@gmail.com>
2021-04-27 11:13:07 +0200

    Format code according to Plone standards: black, isort.

--- recensio/altertum/browser/views.py
+++ recensio/altertum/browser/views.py
@@ -3,8 +3,7 @@ from recensio.theme.browser.pdfgen import GeneratePdfRecension
 
 
 class GeneratePdfRecensionAltertum(GeneratePdfRecension):
-    """Customized cover page
-    """
+    """Customized cover page"""
 
     logo_main = "++resource++recensio.altertum.images/logo2_fuer-Deckblatt.jpg"
 
